44d35a2e416220adc61de66380c9b4b2e5f26495
Evg Chu <chuevgenii@gmail.com>
2022-06-07 22:27:09 +0300

    changed YANDEX_API_KEY

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-
+from local_config import LocalConfig
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    YANDEX_API_KEY = "5c7fa568-dad0-48ed-8a75-c87b879b869b"
+    YANDEX_API_KEY = os.environ.get('YANDEX_API_KEY') or LocalConfig.YANDEX_API_KEY
     LOG_FILE_NAME = ".log"
     
