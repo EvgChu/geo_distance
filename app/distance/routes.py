3c56a1ae0fb3f8445f3fa4bd225f4bfc596deec9
Evg Chu <chuevgenii@gmail.com>
2022-06-07 21:37:49 +0300

    added exception handler

--- app/distance/routes.py
+++ app/distance/routes.py
@@ -2,13 +2,25 @@
 from flask import request, jsonify
 from app.distance import bp
 from config import Config
-from .geoService import YaGeoService
+from .geoService import (YaGeoService,
+                        UnexpectedResponse,
+                        NothingFound,
+                        InvalidKey
+)
 
 geo = YaGeoService(Config.YANDEX_API_KEY)
 
 @bp.route('/')
 def get_distance():
     if "address" in request.args:
-        points = geo.coordinates(request.args["address"])
-        return jsonify({"distance": points})
+        try:
+            points = geo.coordinates(request.args["address"])
+        except (UnexpectedResponse, NothingFound):
+            return jsonify({"Not correct request": points})
+        except InvalidKey:
+            return jsonify({"Server error": points})
+        return jsonify({
+            "distance": points,
+            "address": request.args["address"]
+            })
     return "Ok blueprint"
