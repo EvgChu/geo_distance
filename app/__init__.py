56fbf3793462cef362013f0c0d9f625dfa382c42
Evg Chu <chuevgenii@gmail.com>
2022-06-08 09:38:32 +0300

    added description

--- app/__init__.py
+++ app/__init__.py
@@ -9,5 +9,5 @@ app.config.from_object(Config)
 logging.basicConfig(filename=Config.LOG_FILE_NAME, level=logging.DEBUG)
 
 from app.distance import bp as distance_bp
-app.register_blueprint(distance_bp)
+app.register_blueprint(distance_bp,  url_prefix='/v1')
 
--- test_api.py
+++ test_api.py
@@ -4,7 +4,7 @@ from flask import json
 def test_in_mkad():        
     data = {'address': "москва, красная площадь"}
     response = app.test_client().get(
-        '/',
+        '/v1/',
         query_string = data
     )
 
@@ -18,7 +18,7 @@ def test_close_out_mkad():
     data = {'address': "Реутовская улица, 6"
                 "Балашиха, Московская область"}
     response = app.test_client().get(
-        '/',
+        '/v1/',
         query_string = data
     )
 
@@ -32,7 +32,7 @@ def test_close_out_mkad():
 def test_close_in_mkad():        
     data = {'address': "улица Маршала Прошлякова, 26к3с1 Москва"}
     response = app.test_client().get(
-        '/',
+        '/v1/',
         query_string = data
     )
 
@@ -45,7 +45,7 @@ def test_close_in_mkad():
 def test_out_mkad():        
     data = {'address': "Нижний Новгород, Пискунова 16"}
     response = app.test_client().get(
-        '/',
+        '/v1/',
         query_string = data
     )
 
@@ -58,7 +58,7 @@ def test_out_mkad():
 def test_bad_requst():
     data = {'address': "Нdfs6 sdfx"}
     response = app.test_client().get(
-        '/',
+        '/v1/',
         query_string = data
     )
 
