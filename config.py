191de65c2cd7f2a4dd32529ee6df8a6d338acd69
KinChu <chuevgenii@gmail.com>
2023-09-13 19:40:23 +0300

    some version local config

--- config.py
+++ config.py
@@ -4,10 +4,11 @@ try:
     from local_config import LocalConfig
 except:
     class LocalConfig:
-        YANDEX_API_KEY = ''
+        YANDEX_API_KEY = os.environ.get('YANDEX_API_KEY') or "bad"
+        SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    YANDEX_API_KEY = os.environ.get('YANDEX_API_KEY') or LocalConfig.YANDEX_API_KEY
+    SECRET_KEY = LocalConfig.SECRET_KEY
+    YANDEX_API_KEY = LocalConfig.YANDEX_API_KEY
     LOG_FILE_NAME = ".log"
     
